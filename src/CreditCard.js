e2e8f2eda3b5c47f7507d69b2e470c35535daedd
Ollie Williams <oliverwilliams345@gmail.com>
2019-11-02 12:37:46 +0000

    initial commit.

---
+++ src/CreditCard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './CreditCard.scss';
+
+function CreditCard() {
+    return (
+        <input type="text" inputMode="numeric" className="gg-c-form__input"></input>
+    )
+}
+
+export default CreditCard;
